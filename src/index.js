78be26d5fa42c5d4e853aca54f7767c359f59eaf
Pablo Levy <plevym@gmail.com>
2019-09-04 12:01:15 -0500

    remove node-modules

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ const { ApolloServer, PubSub, gql } = require('apollo-server')
 const RedisServer = require('ioredis')
 const pubsub = new PubSub()
 const redis = new RedisServer()
+const listener = new RedisServer()
 const { typeDefs } = require('./typedefs')
 const { resolvers } = require('./resolvers')
 
+listener.subscribe('PAIR_ADDED')
+
+listener.on('message', (channel, message) => {
+  console.log(`received message ${message} on channel ${channel}`)
+  pubsub.publish(channel, { pairAdded: JSON.parse(message) })
+})
+
 const server = new ApolloServer({
-	typeDefs,
-	resolvers,
+  typeDefs,
+  resolvers,
   context: {
-  	redis,
-  	pubsub,
+    redis,
+    pubsub,
   },
   formatError: error => {
     console.log(error);
@@ -18,6 +26,6 @@ const server = new ApolloServer({
   },
 })
 
-server.listen().then(({url}) => {
-	console.log(`Server ready at ${url}`)
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
 })
